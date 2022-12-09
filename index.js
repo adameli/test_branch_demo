7f1b2958c47fa6f90a36a42b1bae7d22e53f4622
Adam Eliasson <adam.eliasson.swe@gmail.com>
2022-12-09 13:17:26 +0100

    basecode clicking

---
+++ index.js
@@ -0,0 +1,12 @@
+"use strict"
+
+let next = "x"
+let nextturnheading = document.querySelector("h1")
+
+let boxone = document.querySelector("#one")
+
+boxone.addEventListener("click", action)
+
+function action(event) {
+    console.log(event);
+}
