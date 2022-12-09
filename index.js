a7468277354335be9e3798e96647052d53de38d2
Adam Eliasson <adam.eliasson.swe@gmail.com>
2022-12-09 14:14:13 +0100

    complate board check and all win fallouts

--- index.js
+++ index.js
@@ -26,7 +26,30 @@ function action(event) {
 }
 
 function checkifsomeonewon() {
-    if (boxes[0].textContent === boxes[1].textContent && boxes[0].textContent === boxes[2].textContent) {
+    // first, second, third row
+    if (boxes[0].textContent !== "" && boxes[0].textContent === boxes[1].textContent && boxes[1].textContent === boxes[2].textContent) {
+        nextturnheading.textContent = `${boxes[0].textContent} WON !`
+    } else if (boxes[3].textContent !== "" && boxes[3].textContent === boxes[4].textContent && boxes[4].textContent === boxes[5].textContent) {
+        nextturnheading.textContent = `${boxes[3].textContent} WON !`
+    } else if (boxes[6].textContent !== "" && boxes[6].textContent === boxes[7].textContent && boxes[7].textContent === boxes[8].textContent) {
+        nextturnheading.textContent = `${boxes[6].textContent} WON !`
+    }
+    // first, second, third column
+    else if (boxes[0].textContent !== "" && boxes[0].textContent === boxes[3].textContent && boxes[3].textContent === boxes[6].textContent) {
         nextturnheading.textContent = `${boxes[0].textContent} WON !`
     }
+    else if (boxes[1].textContent !== "" && boxes[1].textContent === boxes[4].textContent && boxes[4].textContent === boxes[7].textContent) {
+        nextturnheading.textContent = `${boxes[1].textContent} WON !`
+    }
+    else if (boxes[2].textContent !== "" && boxes[2].textContent === boxes[5].textContent && boxes[5].textContent === boxes[8].textContent) {
+        nextturnheading.textContent = `${boxes[2].textContent} WON !`
+    }
+    // diaginol 1 and 2
+    else if (boxes[0].textContent !== "" && boxes[0].textContent === boxes[4].textContent && boxes[4].textContent === boxes[8].textContent) {
+        nextturnheading.textContent = `${boxes[0].textContent} WON !`
+    }
+    else if (boxes[2].textContent !== "" && boxes[2].textContent === boxes[4].textContent && boxes[4].textContent === boxes[6].textContent) {
+        nextturnheading.textContent = `${boxes[2].textContent} WON !`
+    }
+
 }
